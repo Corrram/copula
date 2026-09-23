32b02247758daf1b0e4fe6ffba299157a9907d46
Marcus Rockel <marcusrockel@icloud.com>
2026-09-23 18:35:33 +0200

    Prove Nelsen 12 infinite-parameter comonotonic endpoint

--- Copula.lean
+++ Copula.lean
@@ -55,6 +55,7 @@ import Copula.Families.Nelsen2Limits
 import Copula.Families.Nelsen7
 import Copula.Families.Nelsen8
 import Copula.Families.Nelsen8Limits
+import Copula.Families.Nelsen12Limits
 import Copula.Families.Clayton.Negative
 import Copula.Archimedean.Symmetry
 import Copula.OrdinalSum
